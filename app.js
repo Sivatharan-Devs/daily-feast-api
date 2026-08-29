f5f816364fbed0ed982893520cc48c84e6600c11
sivatharan-Devs <sivatharan2022@gmail.com>
2026-08-30 00:00:48 +0530

    chore: add Morgan request logging for development

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 
 dotenv.config({ path: './config.env' });
 
@@ -8,5 +9,8 @@ const app = express();
 // Middlewares
 // third-party middleswares
 app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 export default app;
