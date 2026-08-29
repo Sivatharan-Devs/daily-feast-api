b7b21d8bbe546cdb43eec648028186e958e58b7f
sivatharan-Devs <sivatharan2022@gmail.com>
2026-08-29 23:39:03 +0530

    feat: add express and express json parsing middleware

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config({ path: './config.env' });
+
+const app = express();
+
+// Middlewares
+// third-party middleswares
+app.use(express.json());
