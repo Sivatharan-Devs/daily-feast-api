c541f1a3e7b72bb9b01f2397282db2c41c0fcd21
sivatharan-Devs <sivatharan2022@gmail.com>
2026-09-03 18:10:13 +0530

    feat:add mongoose for connect mongodb and app and connect with mongodb

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 // own module import
+import mongoose from 'mongoose';
 import app from './app.js';
 
 const port = process.env.PORT || 3000;
+
+const DB = process.env.DB_CONNECTION_STRING.replace(
+  '<APP_NAME>',
+  process.env.APP_NAME
+)
+  .replace('<DB_USERNAME>', process.env.DB_USERNAME)
+  .replace('<DB_PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful');
+});
 
 app.listen(port, () => {
   console.log('Hello from the server 👋');
